1e2857fc5fa83677b198e8e299b463af27a98cf4
Troy M. LaPolice <55710427+TroyLaPolice@users.noreply.github.com>
2020-05-06 23:12:36 -0400

    Update initial_R_dataAnalysisAndFigures.R

--- initial_R_dataAnalysisAndFigures.R
+++ initial_R_dataAnalysisAndFigures.R
@@ -20,18 +20,18 @@ colnames(duplicated_genes) <- c("Busco id", "Status", "Scaffold", "Start", "End"
 # *****************************************************
 #   Count the number of duplicates on each Scaffold
 # *****************************************************
-  
+
 #This counts the number of duplicates on each Scaffold
 
-scaffold_count <- count(duplicated_genes, "Scaffold")
+scaffold_count <- count(duplicated_genes, Scaffold)
 
 write.csv(scaffold_count, "scaffold_count.csv")
 
 #This makes a density plot of the number of duplicates per scaffold
 
-scaffold_count_dens <- ggplot(scaffold_count, aes(x=Scaffold, fill='Frequency')) + 
-  geom_density(alpha=.3) +
-  ylab("Density") + xlab("Scaffold") +
+scaffold_count_dens <- ggplot(scaffold_count, aes(x=Scaffold, fill=' ')) + 
+  geom_density(alpha=1) +
+  ylab("Density") + xlab("Contig") +
   theme(axis.title = element_text(size=20), axis.text = element_text(size = 14),
         panel.grid.major = element_blank(), panel.grid.minor = element_blank(), 
         panel.background = element_blank(), axis.line = element_line(colour = "black"),
