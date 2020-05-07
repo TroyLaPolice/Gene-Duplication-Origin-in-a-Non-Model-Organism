e322f5f0e5eee1ae52c22a526686bf194e43da81
Troy M. LaPolice <55710427+TroyLaPolice@users.noreply.github.com>
2020-05-07 13:49:21 -0400

    Create MAFFTtest_regions_of_interest.sh

---
+++ MAFFTtest_regions_of_interest.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name=ROIMAFFTtrial
+#SBATCH --output=MAFFT_regions_of_interest_trial.log
+#SBATCH --partition=macmanes
+#SBATCH --exclude=node117,node118
+#SBATCH --mem 115Gb
+
+cd /mnt/lustre/macmaneslab/tml1019/seniorThesis
+
+module load linuxbrew/colsa
+
+mafft --thread 8 --threadtb 5 --threadit 0 --reorder --auto regions_of_interestTEST.fa > MAFF_regions_of_interest_trial_output
