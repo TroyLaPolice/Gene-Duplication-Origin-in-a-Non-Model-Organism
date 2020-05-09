de558de054a35c55fb446952043488ef3c4e9d1d
Troy M. LaPolice <55710427+TroyLaPolice@users.noreply.github.com>
2020-05-09 12:01:40 -0400

    Update MAFFTtest_regions_of_interest-3kb.sh

--- MAFFTtest_regions_of_interest-3kb.sh
+++ MAFFTtest_regions_of_interest-3kb.sh
@@ -9,4 +9,4 @@ cd /mnt/lustre/macmaneslab/tml1019/seniorThesis
 
 module load linuxbrew/colsa
 
-mafft --thread 8 --threadtb 5 --threadit 0 --reorder --auto regions_of_interestTEST.fa > MAFF_regions_of_interest_trial_output-3kb.aln
+mafft --thread 8 --threadtb 5 --threadit 0 --reorder --auto regions_of_interestTEST-3kb.fa > MAFFT_regions_of_interest_trial_output-3kb.aln
