4424c8744be5697984a9258d83b0ca857cb28b6c
Troy M. LaPolice <55710427+TroyLaPolice@users.noreply.github.com>
2020-06-10 10:03:16 -0400

    Update NanoporeFilter-f256.sh

--- NanoporeFilter-f256.sh
+++ NanoporeFilter-f256.sh
@@ -10,4 +10,6 @@ module load linuxbrew/colsa
 
 cd /mnt/lustre/macmaneslab/tml1019/seniorThesis/BWAandDepth/Nanopore_aligned_reads
 
+#the F filters and the -h keeps the header in the file, this is important for later
+
 samtools view -h -F 256 Nanopore.sorted.bam > Nanopore_reads-F256.sam
