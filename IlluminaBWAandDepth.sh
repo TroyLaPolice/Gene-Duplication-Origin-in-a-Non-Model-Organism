3aa917c4ae6756619aefa8b495bd5dac7b61c0ec
Troy M. LaPolice <55710427+TroyLaPolice@users.noreply.github.com>
2020-07-01 10:27:26 -0400

    Update IlluminaBWAandDepth.sh

--- IlluminaBWAandDepth.sh
+++ IlluminaBWAandDepth.sh
@@ -3,7 +3,8 @@
 #SBATCH -J bwaill
 #SBATCH --output illbwa.log
 #SBATCH --cpus-per-task=40
-#SBATCH --exclude node117,node118
+#SBATCH --mem=700000
+#SBATCH --exclude=node117,node118
 
 DIR="/mnt/lustre/macmaneslab/tml1019/seniorThesis/genomeFiles"
 ASSEMBLY="/mnt/lustre/macmaneslab/tml1019/seniorThesis/genomeFiles/imitator.1.3.6.fa"
@@ -18,11 +19,11 @@ echo Running BWA on $ASSEMBLY
 cd /mnt/lustre/macmaneslab/tml1019/seniorThesis/BWAandDepth/Illumina_aligned_reads
 
 #bwa index ${DIR}/${ASSEMBLY}
-bwa mem -t 40 $ASSEMBLY $READ1 $READ2 > illaln_TEMP.sam
+/usr/bin/time -v bwa mem -t 40 $ASSEMBLY $READ1 $READ2 > illaln_TEMP_ReRun.sam
 
-samtools view illaln_TEMP.sam -@20 -Sb \
-| samtools sort -T Illumina -O bam -@20 -l9 -m2G -o Illumina.sorted.bam -
-samtools index Illumina.sorted.bam
+samtools view illaln_TEMP_ReRun.sam -@20 -Sb \
+| samtools sort -T Illumina -O bam -@20 -l9 -m2G -o Illumina_ReRun.sorted.bam -
+samtools index Illumina_ReRun.sorted.bam
 
 # samtools depth
-samtools depth Illumina.sorted.bam > IlluminaDepth.tsv
+samtools depth Illumina_ReRun.sorted.bam > IlluminaDepth_ReRun.tsv
